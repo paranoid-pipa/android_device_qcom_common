14a5f6487f6755201fb75d36c6a69972f98e7ad3
Abhay Singh Gill <abhaygill017@gmail.com>
2023-09-13 02:05:48 +0530

    common: Drop unused dlkm component
    * KERNEL_MODULES_OUT variable from the dlkm component is now replaced by TARGET_USES_DLKM for use in our init component.
    
    Change-Id: I7c8ca9e39263eb452bf142625942cb9ab7f16be9

--- components.mk
+++ components.mk
@@ -76,10 +76,8 @@ ifneq (,$(filter dsprpcd, $(TARGET_COMMON_QTI_COMPONENTS)))
   include $(QCOM_COMMON_PATH)/vendor/dsprpcd/qti-dsprpcd.mk
 endif
 
-ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 ifeq (,$(filter 4.4 4.9 4.14 4.19 5.4, $(TARGET_KERNEL_VERSION)))
-  include $(QCOM_COMMON_PATH)/dlkm/qti-dlkm.mk
-endif
+  TARGET_USES_DLKM ?= true
 endif
 
 ifneq (,$(filter gps, $(TARGET_COMMON_QTI_COMPONENTS)))
